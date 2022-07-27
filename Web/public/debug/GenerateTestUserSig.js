e645995703f5bce1a2c87949202f98b586132146
naturalyuan <naturalyuan@tencent.com>
2022-07-27 19:24:15 +0800

    fix: improve aegis statistics

--- Web/public/debug/GenerateTestUserSig.js
+++ Web/public/debug/GenerateTestUserSig.js
@@ -10,7 +10,7 @@ import * as LibGenerateTestUserSig from './lib-generate-test-usersig.min.js'
  * 进入腾讯云实时音视频[控制台](https://console.cloud.tencent.com/rav ) 创建应用，即可看到 SDKAppId，
  * 它是腾讯云用于区分客户的唯一标识。
  */
-const SDKAPPID = 1400188366
+const SDKAPPID = 0
   
 
 /**
@@ -47,7 +47,7 @@ const EXPIRETIME = 604800
  * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
  * 文档：https://cloud.tencent.com/document/product/647/17275#Server
  */
-const SECRETKEY = '217a9b4a174649a8a41ea7166faa8666e0973a3312ef9b20ad1ad52e9bbb5e94'
+const SECRETKEY = ''
 
 /**
  * Module: GenerateTestUserSig
